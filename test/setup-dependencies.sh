cc39a6846cd066af60bb306f23bbc3171068f6bf
Radoslav Husar <radosoft@gmail.com>
2023-08-10 13:15:46 +0200

    wip

--- test/setup-dependencies.sh
+++ test/setup-dependencies.sh
@@ -9,7 +9,7 @@ cp target/websocket-hello-0.0.1.war $TEST_DIR
 cd ..
 
 # get mod_cluster (Java/Tomcat part)
-git clone https://github.com/modcluster/mod_cluster
+git clone https://github.com/rhusar/mod_cluster
 cd mod_cluster
 mvn install || exit 2
 cd $TEST_DIR
